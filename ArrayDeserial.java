af775e46f73b7ac76a0b2777083e36a7d86d6f62
rossannebossa <47313529+rossannebossa@users.noreply.github.com>
2019-02-06 11:57:28 +0100

    Update ArrayDeserial.java

--- ArrayDeserial.java
+++ ArrayDeserial.java
@@ -8,7 +8,7 @@ public class ArrayDeserial {
     public static void main (String [] args){
         ArrayList<Object> list = new ArrayList<>();
         try{
-            FileInputStream fis = new FileInputStream("C:\\Users\\Rossanne S. Bossa\\Documents\\Files\\output.txt");
+            FileInputStream fis = new FileInputStream("C:\\Users\\Files\\output.txt");
             ObjectInputStream ois = new ObjectInputStream(fis);
             list = (ArrayList) ois.readObject();
             ois.close();
